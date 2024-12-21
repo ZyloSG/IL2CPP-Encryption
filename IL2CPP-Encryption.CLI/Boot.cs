56c1da2da027251107344419e42bb030ddb6199e
Zylo <ohiokardinhong@gmail.com>
2024-12-21 14:06:03 +0100

    First commit

---
+++ IL2CPP-Encryption.CLI/Boot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL2CPP_Encryption.CLI
+{
+    internal class Boot
+    {
+        static void Main(string[] args)
+        {
+        RetryLabel:
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Title = "Il2Cpp Encryptor - github.com/ZyloSG";
+            Console.WriteLine("Welcome, " + Environment.UserName);
+            Console.WriteLine("Enter your global-metadata's file path: ");
+            string filePath = Console.ReadLine();
+            byte[] fileBytes = File.ReadAllBytes(filePath);
+            if (!File.Exists(filePath) || fileBytes == null)
+            {
+                Console.WriteLine("Invalid file path, please try again!");
+                goto RetryLabel;
+            }
+            fileBytes[0] = Cryptography.GenerateByte();
+            fileBytes[1] = Cryptography.GenerateByte();
+            fileBytes[2] = Cryptography.GenerateByte();
+            fileBytes[3] = Cryptography.GenerateByte();
+            fileBytes[4] = Cryptography.GenerateLargeByte();
+            File.WriteAllBytes(filePath.Replace(".dat", "_encrypted.dat"), fileBytes);
+            Console.WriteLine("Done!");
+
+           
+        }
+        
+
+
+    }
+}
---
+++ IL2CPP-Encryption.CLI/Cryptography.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL2CPP_Encryption.CLI
+{
+    public class Cryptography
+    {
+        public static byte GenerateByte()
+        {
+            byte randomByte;
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                byte[] buffer = new byte[1];
+                rng.GetBytes(buffer);
+                randomByte = buffer[0];
+            }
+
+            return randomByte;
+        }
+
+        public static byte GenerateLargeByte()
+        {
+            byte randomByte;
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                do
+                {
+                    byte[] buffer = new byte[1];
+                    rng.GetBytes(buffer);
+                    randomByte = buffer[0];
+
+                } while (randomByte < 100 || randomByte > 255);
+            }
+
+            return randomByte;
+        }
+    }
+}
